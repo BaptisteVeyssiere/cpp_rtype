d7211fe8e5bfa7b60dfced4446eb271c9c0d63ef
ilyas semmaoui <ilyas.semmaoui@epitech.eu>
2018-01-21 01:00:49 +0100

    Adding Protocol changes

--- server/include/Network.hpp
+++ server/include/Network.hpp
@@ -1,23 +1,59 @@
 #pragma once
 
 #include <vector>
+#include "AsioSocket.hpp"
+#include "Protocol.hpp"
+
+struct NetClient
+{
+	unsigned int _id;
+	std::vector<char>	_received;
+	std::vector<char>	_to_send;
+
+	NetClient(unsigned int const &id) : _id(id) {
+
+	}
+};
+
+struct ClientRequest
+{
+	unsigned int					id;
+	std::unique_ptr<Inputs>			input;
+	std::unique_ptr<Connect>		connect;
+};
+
+struct ServerRequest
+{
+	unsigned int							id;
+	std::unique_ptr<Header>					head;
+	std::vector<std::unique_ptr<Entity>>	entities;
+	std::vector<std::unique_ptr<Enemy>>		enemies;
+	std::vector<std::unique_ptr<Player>>	players;
+};
 
 class Network {
 
 private:
 
-	//ISocket	server;
-	//std::vector<ISocket> clientList;
+	AsioSocket	server;
+	std::vector<NetClient>	clientList;
 
 	Network();
 	~Network();
 	Network(Network const &cp) = delete;
 	Network	&operator=(Network const &cp) = delete;
 
+	void	receiveRequest();
+	void	getRequests(std::vector<ClientRequest> &requests, NetClient &client);
+
 public:
 
 	static Network &getInstance();
-	
 
+	void	startReceivingConnections(int const &port);
+	
+	void	getLastRequests(std::vector<ClientRequest>	&requests);
+
+	void	sendRequestsTo(std::vector<ServerRequest> const &requests);
 
 };
--- server/src/Network.cpp
+++ server/src/Network.cpp
@@ -3,7 +3,6 @@
 
 Network::Network()
 {
-
 }
 
 Network::~Network()
@@ -15,4 +14,107 @@ Network	&Network::getInstance()
 {
 	static Network instance;
 	return (instance);
+}
+
+void	Network::startReceivingConnections(int const &port)
+{
+	server.startReceivingConnections();
+}
+
+void	Network::receiveRequest()
+{
+	std::vector<char>	received(1024);
+	unsigned int		size = 0;
+	unsigned int		id = 0;
+	server.receiveRequest(received, size, id);
+	if (id != 0)
+	{
+		bool isNew = true;
+		for (NetClient client : clientList)
+		{
+			if (client._id == id)
+			{
+				if (size > 0)
+					client._received.insert(client._received.end(),
+						received.begin(), received.begin() + size);
+				isNew = false;
+				break;
+			}
+		}
+		if (isNew)
+		{
+			clientList.push_back(NetClient(id));
+			if (size > 0)
+				clientList.back()._received.insert(clientList.back()._received.begin(),
+					received.begin(), received.begin() + size);
+		}
+	}
+}
+
+void	Network::getRequests(std::vector<ClientRequest> &requests, NetClient &client)
+{
+	while (client._received.size() >= sizeof(Inputs)
+		|| client._received.size() >= sizeof(Connect))
+	{
+		uint16_t tmp;
+		std::memcpy(&tmp, client._received.data(), sizeof(tmp));
+		if (tmp == 1 && client._received.size() >= sizeof(Inputs))
+		{
+			std::unique_ptr<Inputs>	move(new Inputs());
+			std::memcpy(move.get(), client._received.data(), sizeof(Inputs));
+			client._received.erase(client._received.begin(), client._received.begin() + sizeof(Inputs));
+			requests.push_back(ClientRequest());
+			requests.back().id = client._id;
+			requests.back().input.reset(move.get());
+		}
+		else if (tmp == 2 && client._received.size() >= sizeof(Connect))
+		{
+			std::unique_ptr<Connect>	connect(new Connect());
+			std::memcpy(connect.get(), client._received.data(), sizeof(Connect));
+			client._received.erase(client._received.begin(), client._received.begin() + sizeof(Connect));
+			requests.push_back(ClientRequest());
+			requests.back().id = client._id;
+			requests.back().connect.reset(connect.get());
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void	Network::getLastRequests(std::vector<ClientRequest> &requests)
+{
+	try {
+		receiveRequest();
+		for (NetClient client : clientList)
+		{
+			if (client._received.size() >= sizeof(Inputs)
+				|| client._received.size() >= sizeof(Connect))
+			{
+				getRequests(requests, client);
+			}
+		}
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+}
+
+void	Network::sendRequestsTo(std::vector<ServerRequest> const &requests)
+{
+	for (int i = 0; i < requests.size(); ++i)
+	{
+		for (NetClient client : clientList)
+		{
+			if (requests[i].id == client._id)
+			{
+
+				//while ()
+				//client._to_send.insert(client._to_send)
+				break;
+			}
+		}
+	}
 }
